0b9b4e3a11e8c34467217aa57fac158f7805db3a
Abdilahi khaliif Hassan <daarood11@hotmail.com>
2021-03-11 09:38:03 +0100

    TodoSequence done

---
+++ TodoIt.Data.Test/TodoSequencer.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace TodoIt.Data.Test
+{
+    public class TodoSequencer
+    {
+        [Fact]
+        public void TodoSqTest()
+        {
+            //Arrange 
+            int PersonId = 0;
+
+            TodoSequencer Id = new TodoSequencer();
+            PersonId = 0;
+            //Asser
+            Assert.NotEqual(1, PersonId);
+
+        }
+    }
+}
---
+++ TodoIt/Data/TodoSequencer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoIt.Data
+{
+       public  class TodoSequencer
+    {
+        private static int PersonId;
+        public static int TodoId()
+        {
+            PersonId++;
+            return PersonId + PersonId;
+            //TodoSequencer Id = new TodoSequencer();
+            //my be
+
+        }
+        public static void Reset()
+        {
+            PersonId = 0;
+        }
+    }
+}
+/*
+ Create a new class TodoSequencer in Data folder that have the same behaviour as
+PersonSequencer but different method names.
+a. Unit test TodoSequencer.
+b. Commit changes.
+ 
+ */
