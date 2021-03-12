a3c02b70bbed6b86ef9ab56ba21b9d61a0d6dee8
Abdilahi khaliif Hassan <daarood11@hotmail.com>
2021-03-12 09:58:30 +0100

    TodoIten is done and tested

---
+++ TodoIt.Data.Test/TodoIten.Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace TodoIt.Data.Test
+{
+    public class TodoIt
+    {
+        [Fact]
+        public void ToditemTets()
+        {
+            //Arrange 
+            int person;
+            int persinId;
+            //Act
+            People array = new People();
+            person = 0;
+            persinId = 0;
+            //Asser
+            Assert.Equal(0, persinId);
+
+        }
+    }
+}
---
+++ TodoIt/Data/TodoItems.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoIt.Data
+{
+    public class TodoItems
+    {
+
+        private static int[] person = new int[]{
+
+        };
+        int sizeOfArray = person.Length;
+        public static int Size()
+        {
+            return person.Length;
+            //sizeof.
+
+        }
+
+        public int FindAll(int[] person)
+        {
+            return sizeOfArray;
+        }
+        public int FindById(int[] personId)
+        {
+            person = personId;
+            return sizeOfArray;
+        }
+        public int CreatePerson(int[] persinId)
+
+        {
+            People array = new People();
+            array.CreatePerson(persinId);
+            // person[0] = 1;
+            //person[1] = 1;
+            Array.Resize(ref person, 10);
+
+            return sizeOfArray;
+
+        }
+        public void Clea()
+        {
+            Array.Clear(person, 0, person.Length);
+        }
+    }
+}
+//TodoItems should have the same
+//functionality as the People class.
